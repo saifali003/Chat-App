761596857ae6846e3d260d5bd78c38e388aca132
Saif Ali <saifalipasha003@gmail.com>
2026-06-20 17:15:55 +0530

    Update index.js

--- Backened/index.js
+++ Backened/index.js
@@ -11,8 +11,9 @@ dotenv.config();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.set("trust proxy", 1);
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://chat-app-frontened-4evv.onrender.com",
     credentials : true
 }));
 app.use(cookieParser());
