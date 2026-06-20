3c54c2d6b91c99bb8c1b93c831814e9493bee52f
Saif Ali <saifalipasha003@gmail.com>
2026-06-20 17:22:43 +0530

    Update auth.controllers.js

--- Backened/controllers/auth.controllers.js
+++ Backened/controllers/auth.controllers.js
@@ -30,8 +30,8 @@ export const signup = async(req,res)=>{
         res.cookie("token",token,{
             httpOnly : true,
             maxAge : 7*24*60*60*1000,
-            sameSite : "Strict",
-            secure : false
+            sameSite : "None",
+            secure : true
         })
         return res.status(201).json({
             message : "User signup successfully",
@@ -72,8 +72,8 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         });
 
         return res.status(200).json({
